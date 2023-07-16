ba631c71d744c0ea61293daa0379690f023b689a
kana <dxrixnd@gmail.com>
2023-07-16 07:44:10 +0100

    Adding the bot to the main branch

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`✅ ${c.user.tag} is now online!`);
+
+    client.user.setActivity({
+        name: `${c.guilds.cache.size} servers`,
+        type: ActivityType.Watching,
+    });
+});
+
+client.login(process.env.TOKEN);
